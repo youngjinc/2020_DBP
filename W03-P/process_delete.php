de7065448338ada5900e7203a4b4920ebb3b9b20
Youngjin Choi <48701368+youngjinc@users.noreply.github.com>
2020-09-17 23:34:27 +0900

    process_delete.php

---
+++ W03-P/process_delete.php
@@ -0,0 +1,21 @@
+<?php
+  $link = mysqli_connect('localhost', 'root', '20181006', 'assignment');
+  settype($_POST['id'], 'int');
+  $filtered = array(
+    'id'=> mysqli_real_escape_string($link, $_POST['id'])
+  );
+
+  $query = "
+    delete from plant
+    where
+      id = '{$filtered['id']}'
+  ";
+
+  $result = mysqli_multi_query($link, $query);
+  if ($result == false) {
+      echo '삭제하는 과정에서 문제가 발생했습니다. 관리자에게 문의하세요.';
+      print mysqli_error($link);
+      error_log(mysqli_error($link));
+  } else {
+      echo '성공했습니다. <a href = "index.php">돌아가기</a>';
+  }
