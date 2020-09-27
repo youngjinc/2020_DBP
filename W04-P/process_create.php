9cfe4785d12dee46a4d70c2a38b6f93ec27791c2
Youngjin Choi <48701368+youngjinc@users.noreply.github.com>
2020-09-27 17:06:00 +0900

    process_create.php

---
+++ W04-P/process_create.php
@@ -0,0 +1,26 @@
+<?php
+  $link = mysqli_connect('localhost', 'root', '20181006', 'assignment');
+  $filtered = array(
+    'title' => mysqli_real_escape_string($link, $_POST['title']),
+    'description' => mysqli_real_escape_string($link, $_POST['description']),
+    'care_id' => mysqli_real_escape_string($link, $_POST['care_id'])
+  );
+
+  $query = "
+  insert into plant(
+    title, description, created, care_id
+    ) value (
+      '{$filtered['title']}',
+      '{$filtered['description']}',
+      now(),
+      '{$filtered['care_id']}'
+      )
+  ";
+
+  $result = mysqli_multi_query($link, $query);
+  if ($result == false) {
+      echo '저장하는 과정에서 문제가 발생했습니다. 관리자에게 문의하세요.';
+      error_log(mysqli_error($link));
+  } else {
+      header("Location: index.php");
+  }
