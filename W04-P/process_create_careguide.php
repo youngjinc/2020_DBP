9ee171378b7860b9c59333c41aaf7f83fe08781d
Youngjin Choi <48701368+youngjinc@users.noreply.github.com>
2020-09-27 17:06:14 +0900

    process_create_careguide.php

---
+++ W04-P/process_create_careguide.php
@@ -0,0 +1,23 @@
+<?php
+  $link = mysqli_connect('localhost', 'root', '20181006', 'assignment');
+  $filtered = array(
+    'type' => mysqli_real_escape_string($link, $_POST['type']),
+    'watering' => mysqli_real_escape_string($link, $_POST['watering'])
+  );
+
+  $query = "
+  insert into care
+    (type, watering)
+  value (
+      '{$filtered['type']}',
+      '{$filtered['watering']}'
+      )
+  ";
+
+  $result = mysqli_query($link, $query);
+  if ($result == false) {
+      echo '저장하는 과정에서 문제가 발생했습니다. 관리자에게 문의하세요.';
+      error_log(mysqli_error($link));
+  } else {
+      header("Location: care.php");
+  }
